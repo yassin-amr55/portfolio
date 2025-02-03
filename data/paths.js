7fbb640f29155d99c154a71d8aef2adb83e01bd5
Yaspro <yasprogames1@gmil.com>
2025-02-03 10:11:13 +0200

    about-us updated

---
+++ data/paths.js
@@ -0,0 +1,27 @@
+const paths = [
+  {
+    date: '7/2024',
+    title: 'Html and Css',
+    disc: 'I started my journey by learning html and css by <a href="https://youtu.be/G3e-cpL7ofc?si=ktMARaYx4-MVXJ3U">SuperSimpleDev</a> whome I give huge respect for his amazing free tutorial starting my programming path.'
+  },
+  {
+    date: '8/2024',
+    title: 'JavaScript',
+    disc: 'I then went along the programming path after mastering html and css by learning javascript by <a href="https://youtu.be/EerdGm-ehJQ?si=Nv14RrofFL_LJmgH">SuperSimpleDev</a> whome I also give a huge respect and shout out as his tutorials made me finally master html, css and javascript in only 2 months.'
+  },
+  {
+    date: '9/2024 - 10/2024',
+    title: 'First projects',
+    disc: 'After mastering html, css and javascript I started my first projects by creating a scientific calculator and few other projects which gave me a bigger understanding, knowledge and experience about web-devolpment.'
+  },
+  {
+    date: '11/2024',
+    title: '1st client/webisite',
+    disc: 'I got my first ever client (SUMMIT) and created a nice website for their company <a href="https://summittecs.com/">summitecs.com</a> which took me a month to complete and I was very happy with the result.'
+  },
+  {
+    date: '12/2024 - 1/2025',
+    title: 'Slow progress',
+    disc: 'In those 2 months I got some more clients but I had to decline and slow my progress as I had studies and exams that I had to focus on but I still learnt html5 Canvas by <a href="https://youtu.be/EO6OkltgudE?si=BhwTRVUPre1qe2ww">Chris Courses</a> who also had some really nice tutorials for html5 Canvas.'
+  }
+]
